0e9aafb41b54a1f3de6dd1cea86a9f83096d8e4a
wmanica <plasticinegiga@gmail.com>
2024-02-11 14:15:05 +0100

    Improve 'user_input' method in UserPromptService
    Refactored the 'user_input' method to handle invalid city inputs more effectively. Now, instead of instantly setting the timezone to a default value when the given input is not found, the method will repeatedly ask for a valid input until it is provided by the user. Additionally, incorporated exit functionality and enhanced error handling.

--- app/services/user_prompt_service.rb
+++ app/services/user_prompt_service.rb
@@ -7,40 +7,55 @@ require 'paint'
 class UserPromptService
   DEFAULT_TZ = 'Berlin'
 
+  def initialize(input_stream = $stdin)
+    @input_stream = input_stream
+  end
+
   def city_prompt
-    content = <<~TEXT
+    puts instruction_text
+    user_input
+    @city_tz
+  end
+
+  def instruction_text
+    <<~TEXT
       PORTUGUESE SPORT TV CHANNELS
 
       Enter the name of the city (for example: London) to convert games date/times into
       ** type #{Paint['help', :green]} for the list of cities/timezones **
+      ** type #{Paint['exit', :red]} to exit the application **
     TEXT
-
-    puts content
-    user_input
   end
 
   def user_input
-    input = STDIN.gets.chomp
-    if input.downcase == 'help'
-      cities_tz_list
-    else
-      @city_tz = find_tzinfo(input.capitalize)
-      # If @city_tz is not correctly initialised, initialise with default value
-      @city_tz ||= ActiveSupport::TimeZone.find_tzinfo(DEFAULT_TZ)
+    input = @input_stream.gets.chomp
+    while invalid_city?(input)
+      if input.downcase == 'exit'
+        Kernel.exit
+      elsif input.downcase == 'help'
+        cities_tz_list
+        input = @input_stream.gets.chomp
+      else
+        puts "\n\n#{Paint['We could not find in our timezone list:', :red]} #{input.downcase}\n\n"
+        input = @input_stream.gets.chomp
+      end
     end
+    @city_tz = find_tzinfo(input.split.map(&:capitalize).join(' '))
+  end
+
+  def invalid_city?(input)
+    find_tzinfo(input.capitalize).nil?
+  rescue TZInfo::InvalidTimezoneIdentifier
+    true
   end
 
   def cities_tz_list
     ActiveSupport::TimeZone.all.sort_by(&:name).map { |o| puts o.name }
-    city_prompt
+    puts "\n\n"
   end
 
   def find_tzinfo(input)
-    return ActiveSupport::TimeZone.find_tzinfo(DEFAULT_TZ) if input.blank?
-
+    return ActiveSupport::TimeZone.find_tzinfo(DEFAULT_TZ) if input.strip.empty?
     ActiveSupport::TimeZone.find_tzinfo(input)
-  rescue TZInfo::InvalidTimezoneIdentifier
-    puts "\n\n#{Paint['We could not find in our timezone list:', :red]} #{input}"
-    city_prompt
   end
 end
