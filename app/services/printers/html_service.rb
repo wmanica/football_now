ec7d35ea15adeb516a0fae3c4ca7c89c7b15be56
wmanica <plasticinegiga@gmail.com>
2024-03-28 02:53:34 +0100

    Add Printers::HtmlService and Printers::RssService with tests
    Introduced two new service classes, Printers::HtmlService and Printers::RssService, with their corresponding rspec test files. Each service class holds its own implementation of the `print_game` and `parse_date_teams` methods, catering to different game data formats, providing more flexibility and maintaining clear separation of concerns.

---
+++ app/services/printers/html_service.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Printers
+  class HtmlService < ::PrintersManagerService
+    def print_game(game)
+      date, time, teams = parse_date_teams(game[:info])
+
+      puts "#{offset(date, time)} #{Paint[game[:tv], :white, :italic]}#{TV_SEPARATOR}#{Paint[colorize_my_team(teams), :bold]}\n"
+    end
+
+    private
+
+    def parse_date_teams(game_info)
+      date, remaining_info = game_info.match(/(\d{1,2}\/\d{1,2})(.*)/).captures
+      time, teams = remaining_info.strip.split(/\s{2,}/, 2)
+      time = "-" if time.empty?
+      [date, time, teams]
+    end
+  end
+end
