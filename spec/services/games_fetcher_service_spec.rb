1483f3f3f5a552c6d454c7b1aa27e0cb0bcf3d57
wmanica <plasticinegiga@gmail.com>
2024-03-28 02:50:16 +0100

    Enhance GamesFetcherService to handle RSS and HTML sources
    Refactored GamesFetcherService to accommodate both RSS and HTML data sources. This involved rewriting methods that handle both sources separately, creating a more flexible and convenient way of fetching game data. The corresponding test file was also updated to better reflect these changes, adding separate contexts for each data service.

--- spec/services/games_fetcher_service_spec.rb
+++ spec/services/games_fetcher_service_spec.rb
@@ -1,41 +1,98 @@
 require 'spec_helper'
 
 describe GamesFetcherService do
-  let(:games_data) { 'fake_games_data' }
-  let(:response) { instance_double(HTTParty::Response, success?: true, parsed_response: { 'rss' => { 'channel' => { 'item' => games_data } } }) }
-  let(:failed_response) { instance_double(HTTParty::Response, success?: false, code: '404') }
+  let(:rss_data) { { 'rss' => { 'channel' => { 'item' => 'fake_rss_data' } } } }
+  let(:html_data) { { 'html' => { 'body' => { 'some_html_structure' => 'fake_html_data' } } } }
+  let(:failed_response) { instance_double(HTTParty::Response, success?: false, code: 404) }
 
   describe '.fetch_games' do
-    context 'when the request is successful' do
+    context 'when the service is RSS' do
+      let(:response) { instance_double(HTTParty::Response, success?: true, parsed_response: rss_data) }
+
       before do
-        allow(HTTParty).to receive(:get).with(described_class::BASE_URL).and_return(response)
+        allow(HTTParty).to receive(:get).with(described_class::RSS_URL).and_return(response)
+        allow(HTTParty).to receive(:get).with(described_class::HTML_URL).and_return(failed_response)
       end
 
-      it 'returns the games data' do
-        expect(described_class.fetch_games).to eq(games_data)
+      it 'returns the games data with service' do
+        expect(described_class.fetch_games).to eq({ games: 'fake_rss_data', service: :Rss })
       end
     end
 
-    context 'when the request fails' do
-      before do
-        allow(HTTParty).to receive(:get).with(described_class::BASE_URL).and_return(failed_response)
+    context 'when the service is HTML' do
+      let(:expected_html_return) do
+        {
+          games: [
+            { info: "Some other data", tv: nil}
+          ],
+          service: :Html
+        }
       end
 
-      it 'raises an exception' do
-        expect { described_class.fetch_games }.to raise_error(RuntimeError, 'Failed to get games. Status code: 404')
+      let!(:response) do
+        instance_double(
+          HTTParty::Response,
+          success?: true,
+          body: %{
+            <html>
+              <body>
+                <h2 class="header">Header</h2>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td><div class="micrologo_and_text"><div class="text"><a>Fake game code</a></div></div></td>
+                      <td>Some other data</td>
+                      <td><img alt="tv info"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </body>
+            </html>
+          }
+        )
+      end
+
+      before do
+        allow(HTTParty).to receive(:get).with(described_class::HTML_URL).and_return(response)
+        allow(HTTParty).to receive(:get).with(described_class::RSS_URL).and_return(failed_response)
+      end
+
+      it 'returns the games data with service' do
+        expect(described_class.fetch_games).to eq(expected_html_return)
       end
     end
 
-    context 'when there is a network error' do
-      let(:error_message) { 'Check your internet connection. Error message:' }
-
+    context 'when the request fails for all' do
       before do
-        allow(HTTParty).to receive(:get).with(described_class::BASE_URL).and_raise(SocketError)
-        allow(Paint).to receive(:[]).and_return(error_message)
+        allow(HTTParty).to receive(:get).with(described_class::HTML_URL).and_return(failed_response)
+        allow(HTTParty).to receive(:get).with(described_class::RSS_URL).and_return(failed_response)
       end
 
       it 'outputs an error message' do
-        expect { described_class.fetch_games }.to output(include(error_message)).to_stdout
+        begin
+          described_class.fetch_games
+        rescue SystemExit => e
+          expect(e.status).to_not eq(0)
+        end
+      end
+
+      it 'returns games and service as nil' do
+        begin
+          result = described_class.fetch_games
+        rescue SystemExit => _
+          expect(result).to be_nil
+        end
+      end
+    end
+
+    context 'when there is an internet connection error' do
+      before do
+        allow(HTTParty).to receive(:get).and_raise(SocketError)
+      end
+
+      it 'returns games and service as nil' do
+        result = described_class.fetch_games rescue nil
+        expect(result).to be_nil
       end
     end
   end
