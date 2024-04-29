7452e9b923857f63aaae598abc7e2d40edf09c35
wmanica <35302092+wmanica@users.noreply.github.com>
2024-04-29 21:02:31 +0200

    Refactor RssService and its tests
    Service now correctly parses and prints game information based on the title, including handling missing titles. It shifted from a simple placeholder message to a more comprehensive display, including date, time, teams and tv channel. This also introduces more comprehensive testing, testing different scenarios of title formats.

--- spec/services/printers/rss_service_spec.rb
+++ spec/services/printers/rss_service_spec.rb
@@ -1,13 +1,31 @@
-# spec/services/printers/rss_service_spec.rb
 RSpec.describe Printers::RssService do
   let(:city_tz) { ActiveSupport::TimeZone["London"] }
-  let(:game) { {'title' => 'Some Game - Game Detail', 'pubDate' => '01 Apr 2023 15:10:10 GMT'} }
 
   subject(:service) { described_class.new([game], city_tz) }
 
   describe '#print_game' do
-    it 'prints the correct message' do
-      expect { service.print_game(game) }.to output("THIS NEED TO BE REFACTORED\n").to_stdout
+    context 'when the title has a proper format' do
+      let(:game) { {'title' => 'FC Genoa x Cagliari FC - 29/04 19:45 - SportTv3', 'pubDate' => '01 Apr 2023 15:10:10 GMT'} }
+
+      it 'prints the correct game info' do
+        expect { service.print_game(game) }.to output(/29\/04 19:45.*SportTv3.*FC Genoa x Cagliari FC/).to_stdout
+      end
+    end
+
+    context 'when the title has only one digit for date and time' do
+      let(:game) { {'title' => 'FC Genoa x Cagliari FC - 9/4 1:9 - SportTv3', 'pubDate' => '01 Apr 2023 15:10:10 GMT'} }
+
+      it 'prints the correct game info' do
+        expect { service.print_game(game) }.to output(/09\/04 01:09.*SportTv3.*FC Genoa x Cagliari FC/).to_stdout
+      end
+    end
+
+    context 'when the title is missing' do
+      let(:game) { {'title' => '', 'pubDate' => '01 Apr 2023 15:10:10 GMT'} }
+
+      it 'does not print anything' do
+        expect { service.print_game(game) }.not_to output.to_stdout
+      end
     end
   end
 end
