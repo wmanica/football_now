166c02fe5f602997266574b8e38e598a73e9cf46
Walter Manica <35302092+wmanica@users.noreply.github.com>
2024-11-27 01:49:42 +0100

    Set games list to 50

--- app/services/printers_manager_service.rb
+++ app/services/printers_manager_service.rb
@@ -17,7 +17,7 @@ class PrintersManagerService
 
     print_city_tz
 
-    games = self.is_a?(Printers::HtmlService) ? @games.take(45) : @games
+    games = self.is_a?(Printers::HtmlService) ? @games.take(50) : @games
 
     games.each do |game|
       print_game(game)
